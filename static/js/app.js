f535cc2f42dcd23c7b7f74ac08c6818f5372e7e9
nadiarichards <73898387+nadiarichards@users.noreply.github.com>
2021-03-08 23:37:38 -0500

    html

--- static/js/app.js
+++ static/js/app.js
@@ -9,7 +9,7 @@ sample_values=data.samples[0].sample_values;
 otu_ids=data.samples[0].otu_ids;
 
 function init() {
-    d3.selectAll("div#bar").node();
+    d3.selectAll("div#bar").html("");
     var trace = {
         x: sample_values,
         y: otu_ids,

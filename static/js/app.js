acbd23dee6c4fd105f9204040e9209d0ee09e68a
nadiarichards <73898387+nadiarichards@users.noreply.github.com>
2021-03-11 15:13:38 -0500

    gauge added

--- static/js/app.js
+++ static/js/app.js
@@ -1,54 +1,103 @@
-var dropdownMenu = d3.select("#selDataset");
-var id_input = dropdownMenu.node().value;
-var barChart = d3.selectAll("#bar").node();
 
-buildChart (barChart, data);
+// var id_input = dropdownMenu.node().value;
+// var barChart = d3.selectAll("#bar").node();
 
-function buildChart(id_input) {
+// buildChart (barChart, data);
 
-    d3.json('../samples.json').then((data) => {
-        data.samples.forEach (function(val) {
-            var id = val.id,
-            var sample_values = val.map(sample_value => val.sample_values),
-            var otu_ids = val.map(otu_id => val.otu_ids),
-            var otu_labels = val.map(otu_label => val.otu_labels);
-            console.log(id, sample_values, otu_ids, otu_labels);
-    });
+function buildChart(id) {
+
+    d3.json("samples.json").then((data) => {
+        var ids = data.samples[0].otu_ids;
+        var sampleValues = data.samples[0].sample_values.slice(0,10).reverse();
+        var labels = data.samples[0].otu_labels.slice(0,10);
+        var OTU_top = (data.samples[0].otu_ids.slice(0,10)).reverse();
+        var OTU_id = OTU_top.map(d => "OTU" + d);
+        var labels = data.samples[0].otu_labels.slice(0,10);
 
         var trace = {
-            x: sample_values,
-            y: otu_ids,
+            x: sampleValues,
+            y: OTU_id,
+            text: labels,
+            marker: {
+                color: 'blue'
+            },
             orientation: 'h',
             type: 'bar'
         };
-        var data = [trace];
-        var layout = {title: "Belly Button Biodiversity"};
-    
-    Plotly.newPlot("#bar", data, layout);
 
+        var data = [trace];
+        var layout = {title: "Top 10 OTU" , yaxis: {tickmode: "linear"}, margin: {l:100, r:100, t:100, b:30}
+        };
+    
+    Plotly.newPlot("bar", data, layout);
+
+        var trace1 = {
+            x: data.samples[0].otu_ids,
+            y: data.samples[0].sample_values,
+            mode: 'markers',
+            marker: {
+            color: data.samples[0].otu_ids,
+            size: data.samples[0].sample_values,
+            },
+            text: data.samples[0].otu_labels
+        };
+        
+        var layout1 = {
+            xaxis: {title: "OTU ID"},
+            height: 600,
+            width: 1000
+        };
+
+        var data1 = [trace1];
+        
+    Plotly.newPlot("bubble", data1, layout1);
+    });
+}
+
+function getDemoInfo(id) {
+    d3.json("samples.json").then((sampledata)=> {
+       var metadata = sampledata.metadata;
+       var result = metadata.filter(meta => meta.id.toString() === id)[0];
+       var demographicInfo = d3.select("#sample-metadata");
+       demographicInfo.html("");
+        Object.entries(result).forEach((key) => {
+            demographicInfo.append("h5").text(key[0].toUpperCase() + ": " + key[1] + "\n");
+        });
+        showGauge(result.wfreq);
+    });
+}
+
+function optionChanged(id) { 
+    buildChart(id);
+    getDemoInfo(id);
+}
+
+function init() { 
+    var dropdown = d3.select("#selDataset");
+    d3.json("samples.json").then ((sampledata) => {
+        sampledata.names.forEach(function(name) {
+            dropdown.append("option").text(name).property("value");
+        });
+        buildChart(sampledata.names[0]),
+        getDemoInfo(sampledata.names[0]);
+    });
+}
+
+function showGauge(freq) {
+    console.log("FRQ",freq);
+  var data = [
+      {
+        domain: { x: [0, 1], y: [0, 1] },
+        value: freq,
+        title: { text: "Speed" },
+        type: "indicator",
+        mode: "gauge+number",
+        delta: { reference: 400 },
+        gauge: { axis: { range: [null, 9] } }
+      }
+    ];
+    var layout = { width: 600, height: 400 };
+    Plotly.newPlot('gauge', data, layout);
 };
 
-d3.selectAll("#selDataset").on("change", bulidChart);
-
-// bubblechart code from plotly documentation
-var trace1 = {
-    x: [1, 2, 3, 4],
-    y: [10, 11, 12, 13],
-    mode: 'markers',
-    marker: {
-      color: ['rgb(93, 164, 214)', 'rgb(255, 144, 14)',  'rgb(44, 160, 101)', 'rgb(255, 65, 54)'],
-      opacity: [1, 0.8, 0.6, 0.4],
-      size: [40, 60, 80, 100]
-    }
-  };
-  
-  var data = [trace1];
-  
-  var layout = {
-    title: 'Marker Size and Color',
-    showlegend: false,
-    height: 600,
-    width: 600
-  };
-  
-  Plotly.newPlot('myDiv', data, layout);
+init();

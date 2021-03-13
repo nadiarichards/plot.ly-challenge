c88d4bb44559bf3457e3f14fa54319a0fc458f76
nadiarichards <73898387+nadiarichards@users.noreply.github.com>
2021-03-13 02:24:53 -0500

    gauge finished for now

--- static/js/app.js
+++ static/js/app.js
@@ -78,60 +78,35 @@ function init() {
 }
 
 function showGauge(freq) {
-    var level = freq/9*180;
-    var degrees = 180 - level, radius = .5;
-    var radians = degrees * Math.PI / 180;
-    var x = radius * Math.cos(radians);
-    var y = radius * Math.sin(radians);
-    var mainPath = 'M -.0 -0.025 L .0 0.025 L ',
-     pathX = String(x),
-     space = ' ',
-     pathY = String(y),
-     pathEnd = ' Z';
-    var path = mainPath.concat(pathX,space,pathY,pathEnd);
+    var data = [{
+        domain: { x: [0, 1], y: [0, 1] },
+        value: freq,
+        title: { text: "<b>Belly Button Washing Frequency</b><br><sub>Scrubs per Week</sub>"}, 
+        type: "indicator",
+        mode: "gauge+number",
+        gauge: { axis: { range: [null, 9]},
+        borderwidth: 2,
+        bordercolor: "transparent",
+        steps: [
+          { range: [0, 1], color: "#f7f2ec" },
+          { range: [1, 2], color: "#f3f0e5" },
+          { range: [2, 3], color: "#e9e7c9" },
+          { range: [3, 4], color: "#e5e9b1" },
+          { range: [4, 5], color: "#d5e595" },
+          { range: [5, 6], color: "#b7cd8b" },
+          { range: [6, 7], color: "#87c080" },
+          { range: [7, 8], color: "#85bc8b" },
+          { range: [8, 9], color: "#80b586" }
+            ],
+    //  } }
+        }
+    }];
 
-    var data = [{ type: 'scatter',
-    x: [0], y:[0],
-     marker: {size: 28, color:'850000'},
-     showlegend: false,
-     name: 'WFREQ',
-     text: level,
-     hoverinfo: 'name'
-   },
-   { values: [50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50/9,50/9,50/9,50],
-   rotation: 90,
-   text: ['8-9','7-8','6-7','5-6','4-5','3-4','2-3','1-2','0-1'],
-   textinfo: 'text',
-   textposition:'inside',
-   marker: {colors:['rgba(10, 20, 0, .5)','rgba(44, 157, 10, .5)', 'rgba(110, 184, 42, .5)',
-                          'rgba(170, 202, 42, .5)', 'rgba(202, 209, 95, .5)',
-                          'rgba(210, 206, 145, .5)', 'rgba(232, 226, 202, .5)',
-                          'rgba(242, 226, 202, .5)','rgba(252, 236, 202, .5)',
-                          'rgba(255, 255, 255, 0)']},
-   labels: ['8-9','7-8','6-7','5-6','4-5','3-4','2-3','1-2','0-1'],
-   hoverinfo: 'label',
-   hole: .5,
-   type: 'pie',
-   showlegend: false
- }];
- 
- var layout = {
-   shapes:[{
-       type: 'path',
-       path: path,
-       fillcolor: '850000',
-       line: {
-         color: '850000'
-       }
-     }],
-   title: 'Belly Button Washing Frequency<br>Scrubs per Week',
-   height: 500,
-   width: 500,
-   xaxis: {zeroline:false, showticklabels:false,
-              showgrid: false, range: [-1, 1]},
-   yaxis: {zeroline:false, showticklabels:false,
-              showgrid: false, range: [-1, 1]}
- };
+    var layout = { 
+      width: 500, 
+      height: 400,
+      margin: { t: 0, b: 0 },
+    };
     Plotly.newPlot('gauge', data, layout);
 };
 
